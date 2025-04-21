cd766d37268c672e6af82f54a86e988e751aa57f
Lucas Guerreiro Cantarelli <lgcantarelli@gmail.com>
2025-04-21 11:04:34 -0300

    feat(2024/2): add execution time

--- 2024/day_2/red_nosed_reports.rb
+++ 2024/day_2/red_nosed_reports.rb
@@ -15,7 +15,11 @@ class RedNosedReports
   end
 
   def print
+    init = Time.now
     p "Safe Reports Count: #{safe_reports_count}"
+
+    time = Time.now - init
+    p "Executed in #{time * 1000}ms"
   end
 end
 
