a38c9d7f28ca545929a04f86e744dc93680633c7
Lucas Guerreiro Cantarelli <lgcantarelli@gmail.com>
2025-04-23 23:39:23 -0300

    feat(2024/2): apply problem dampener

--- 2024/day_2/report/levels_safety.rb
+++ 2024/day_2/report/levels_safety.rb
@@ -1,25 +1,41 @@
 class Report
   module LevelsSafety
     def safe?
-      (increasing_levels? || decreasing_levels?) && !outlier_level_found?
+      return true unless problematic_levels?(@levels)
+      safe_dampening_one_problematic_level?
     end
 
     private
 
-      def increasing_levels?
-        @levels.sort == @levels
+      def safe_dampening_one_problematic_level?
+        @levels.each_with_index do |_, index|
+          levels = @levels.dup
+          levels.delete_at(index)
+
+          return true unless problematic_levels?(levels)
+        end
+
+        false
       end
 
-      def decreasing_levels?
-        descending_sorted_levels = @levels.sort_by { |level| -level }
-        descending_sorted_levels == @levels
+      def problematic_levels?(levels)
+        (!increasing_levels?(levels) && !decreasing_levels?(levels)) || outlier_level_found?(levels)
       end
 
-      def outlier_level_found?
+      def increasing_levels?(levels)
+        levels.sort == levels
+      end
+
+      def decreasing_levels?(levels)
+        descending_sorted_levels = levels.sort_by { |level| -level }
+        descending_sorted_levels == levels
+      end
+
+      def outlier_level_found?(levels)
         outlier_found = false
 
-        @levels.each_with_index do |level, index|
-          next_level = @levels[index + 1]
+        levels.each_with_index do |level, index|
+          next_level = levels[index + 1]
 
           if next_level
             level_difference = (level - next_level).abs
